d30375515c940176c235f76648b01c4d9d9b57b5
Georgy Davityan <georgy.davityan@gmail.com>
2016-07-16 17:19:25 +0300

    Initial Commit

---
+++ src/main/java/cc/caucas/bot/telegram/TelegramBotApplication.java
@@ -0,0 +1,12 @@
+package cc.caucas.bot.telegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotApplication.class, args);
+	}
+}
